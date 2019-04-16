8154cd0c79b3c3ef01001a3ae66166fa9398a9b1
dhwajad <dhwajad@gmail.com>
2019-04-16 11:24:29 -0600

    spring mvc exercise

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -10,30 +10,19 @@ import java.util.Map;
 @RestController
 public class EnvController {
 
-    private String port;
-    private String memoryLimit;
-    private String cfInstanceIndex;
-    private String cfInstanceAddr;
+    private Map env = new HashMap<String, String>();
 
-    public EnvController(@Value("${port:NOT SET}") String port,
-                         @Value("${memory.limit:NOT SET}") String memoryLimit,
-                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
-                         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr){
-        this.port = port;
-        this.memoryLimit = memoryLimit;
-        this.cfInstanceIndex = cfInstanceIndex;
-        this.cfInstanceAddr = cfInstanceAddr;
-    }
-
-    @GetMapping("/env")
-    public Map<String, String> getEnv(){
-        HashMap<String, String> env = new HashMap<String, String>();
-
+    public EnvController(@Value("${port:NA}") String port, @Value("${memory.limit:NA}") String memoryLimit,
+                         @Value("${cf.instance.index:NA}") String cfInstanceIndex, @Value("${cf.instance.addr:NA}") String cfInstanceAddress) {
         env.put("PORT", port);
         env.put("MEMORY_LIMIT", memoryLimit);
         env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
-        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddress);
+    }
 
+    @GetMapping("/env")
+    public Map getEnv() {
         return env;
     }
+
 }
